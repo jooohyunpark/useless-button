2b0b4a770720e47380b0f89d31ba1da17a925677
Joohyun Park <jhp527@nyu.edu>
2023-07-15 00:08:22 -0400

    init three

--- src/component/UselessButton/index.js
+++ src/component/UselessButton/index.js
@@ -1,5 +1,13 @@
 import { useEffect, useState } from "react";
-import { Button } from "./styles";
+import { Canvas, useThree } from "@react-three/fiber";
+import {
+  Cylinder,
+  OrbitControls,
+  OrthographicCamera,
+  Sphere,
+} from "@react-three/drei";
+import { motion } from "framer-motion-3d";
+import { MotionConfig } from "framer-motion";
 
 export default function UselessButton() {
   const [checked, setChecked] = useState(false);
@@ -23,5 +31,35 @@ export default function UselessButton() {
     };
   }, [checked]);
 
-  return <Button checked={checked} onChange={handleChange} disableRipple />;
+  return (
+    <Canvas shadows dpr={[1, 2]} resize={{ scroll: false, offsetSize: true }}>
+      <OrthographicCamera makeDefault position={[10, 10, 10]} />
+      <OrbitControls makeDefault maxPolarAngle={Math.PI * 0.5} />
+      <ambientLight intensity={0.3} />
+      <directionalLight position={[-1, 1, 1]} />
+      <axesHelper args={[100]} />
+
+      <MotionConfig
+        transition={{
+          type: "spring",
+          duration: 0.7,
+          bounce: 0.2,
+        }}
+      >
+        <motion.group
+          initial={false}
+          // animate={isHover ? "hover" : "rest"}
+          dispose={null}
+          // variants={{
+          //   hover: { z: isPress ? -0.9 : 0 },
+          // }}
+        >
+          <Sphere args={[1, 32, 32]} />
+          <Cylinder args={[5, 5, 10, 128]}>
+            <meshStandardMaterial color={"orange"} />
+          </Cylinder>
+        </motion.group>
+      </MotionConfig>
+    </Canvas>
+  );
 }
